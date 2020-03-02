eb905ba38eb7def4a85663edefde05f6b5bdc97d
Ho Chien <ChienHos@outlook.com>
2020-03-02 15:36:28 +0700

    Add new Connectton string

---
+++ StudentMangements/StudentManagemet.Core/Models/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagemet.Core.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext():base("StudentConnStr")
+        {
+            Database.SetInitializer<StudentContext>(new StudentInitilier());
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
---
+++ StudentMangements/StudentManagemet.Core/Models/StudentInitilier.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace StudentManagemet.Core.Models
+{
+    internal class StudentInitilier : DropCreateDatabaseAlways<StudentContext>
+    {
+        protected override void Seed(StudentContext context)
+        {
+            List<Grade> Grade = new List<Grade>
+            {
+                new Grade
+                {
+                    GradeName ="Grade1"
+                },
+                  new Grade
+                {
+                    GradeName ="Grade2"
+                }
+            };
+            List<Student> student = new List<Student>
+            {
+                new Student
+                {
+                    StudentAge = 12,
+                    StudentId = 1,
+                    StudentName = "name 1"
+                },
+                  new Student
+                {
+                    StudentAge = 12,
+                    StudentId = 1,
+                    StudentName = "name 2"
+                }
+            };
+            base.Seed(context);
+        }
+    }
+}
