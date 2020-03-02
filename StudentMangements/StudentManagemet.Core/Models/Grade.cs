ab9d96e9949d2e1999840f9fa3e2454b291a5ba7
Ho Chien <ChienHos@outlook.com>
2020-03-02 15:28:30 +0700

    ChienHV1 add new model

---
+++ StudentMangements/StudentManagemet.Core/Models/Grade.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace StudentManagemet.Core.Models
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public ICollection<Student> Student { get; set; }
+    }
+}
